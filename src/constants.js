b2c2f0845fb9ff8e19ffe01b38945691d48b18af
li935 <bandits0726@gmail.com>
2022-06-07 14:27:52 +0900

    feat: Common Code Processing
    - 루트파일 수정 (strict mode 제거, 불필요한 주석 삭제)
    - 공통 폰트 작성
    - constants 작성
    - 루트 구조 확립 (Header, Body, Footer)

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const LINK = {
+    LINKED_IN: "https://www.linkedin.com/in/%EB%8F%99%ED%98%84-%EA%B9%80-521247235/",
+    GITHUB: "https://github.com/li935",
+    NOTION: "https://jamesdonghyunkim.notion.site/jamesdonghyunkim/Donghyun-Kim-bcc26f23239540a9b495996b55467e9f",
+}
---
+++ src/font.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const CustomHeaderFont = styled.span`
+  color: hsla(101.3, 96.1%, 40%, 0.65);
+  font-size: 1.3em;
+  font-style: italic;
+  font-family: Courier Prime, Source Code Pro, monospace;
+  cursor: pointer;
+  
+  &:hover {
+    color: blue;
+  }
+`
+
+export const FooterDescFont = styled.span`
+  color: hsla(101.3, 96.1%, 40%, 0.65);
+  font-family: Courier Prime, Source Code Pro, monospace;
+`
